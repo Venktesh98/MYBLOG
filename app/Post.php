262f7aef7115d1cdec6124ff640eb90b718888b2
venktesh <venkysoma1998@gmail.com>
2020-06-17 15:44:50 +0530

    Post model modified by adding the imageurl accessor

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Defining the Accesssor for image attribute over here that is called when model is called 
+
+    public function getImageUrlAttribute($value)
+    {
+        $imageUrl = "";
+
+        if(! is_null($this->image)) # image is the attribute in the database.
+        {
+            $imagePath = public_path() . "/img/" . $this->image;
+            if(file_exists($imagePath)) 
+            {
+                $imageUrl = asset("img/" . $this->image); # gets the relevant path of the image if the image exists.
+                
+            }
+        }
+        return $imageUrl;
+    }
 }
