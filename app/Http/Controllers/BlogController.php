bdf36f83e9f3e4c494c8e61be97586a57bd71212
venktesh <venkysoma1998@gmail.com>
2020-06-18 13:20:54 +0530

    added pagination

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,10 +7,17 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    protected $author = ['name'];
+    
     public function index()
     {
-        $post = Post::all();
+        // $post = Post::all();
+        // \DB::enableQueryLog();
+        $post = Post::with('author')->latestFirst()->paginate(3);
+        // $post = Post::orderBy('created_at','asc')->paginate(5);
         return view('blog.index')->with('posts_send',$post);
         // dd("blog msg");
+        // dd(\DB::getQueryLog());
+
     }
 }
