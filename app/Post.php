68c21a5dc01b5a36a24f7942e683c853f8832802
venktesh <venkysoma1998@gmail.com>
2020-06-18 17:04:40 +0530

    post model modified by adding the accessor function for the markdowning the body and excerpt attributes

--- app/Post.php
+++ app/Post.php
@@ -2,8 +2,9 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;   # for Markdown i.e for html presentation of contents on the blog
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon; 
+use Carbon\Carbon;                              # for date and time had use the carbon api.
 
 class Post extends Model
 {
@@ -14,27 +15,43 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
     
-    // Defining the Accesssor for image attribute over here that is called when model is called 
+    // Defining the Accesssor function for image attribute over here that is called when model is called 
     public function getImageUrlAttribute($value)
     {
         $imageUrl = "";
 
         if(! is_null($this->image)) # image is the attribute in the database.
         {
-            $imagePath = public_path() . "/img/" . $this->image;
+            $imagePath = public_path() . "/img/" . $this->image;  # public_path() is the helper function to get the path of the public directory
             if(file_exists($imagePath)) 
             {
-                $imageUrl = asset("img/" . $this->image); # gets the relevant path of the image if the image exists.
+                $imageUrl = asset("img/" . $this->image); # gets the relevant path of the image if the image exists using asset().
 
             }
         }
         return $imageUrl;
     }
 
+    // Accessor Function
     public function getDateAttribute($value)
     {
-        return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();     # will creats the date for human readble form.
+        // return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();     # will creats the date for human readble form.
+        return is_null($this->created_at) ? '' : $this->created_at->diffForHumans();
     }
+
+    // Accessor Function
+    public function getBodyHtmlAttribute($value)
+    {
+         return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;  # e is laravel helper function that is used for security purpose.
+    }
+
+    // Accessor Function
+    public function getExcerptHtmlAttribute($value)
+    {
+         return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;  # e is laravel helper function that is used for security purpose.
+    }
+
+    // Scope
     public function scopeLatestFirst($query)
     {
         return $query->orderBy('published_at','desc');
