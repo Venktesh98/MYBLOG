86b983f5f0ffd7f7079447493b63da0fbe7df052
venktesh <venkysoma1998@gmail.com>
2020-06-18 14:34:27 +0530

    added the functionality for the show method

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,12 +12,19 @@ class BlogController extends Controller
     public function index()
     {
         // $post = Post::all();
-        // \DB::enableQueryLog();
+        // \DB::enableQueryLog();    # for debugging the database query
+
         $post = Post::with('author')->latestFirst()->paginate(3);
         // $post = Post::orderBy('created_at','asc')->paginate(5);
         return view('blog.index')->with('posts_send',$post);
         // dd("blog msg");
         // dd(\DB::getQueryLog());
 
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('blog.show')->with("posts",$post);
     }
 }
