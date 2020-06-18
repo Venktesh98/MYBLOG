0d95df4ae7827361e3b9b3a859bce3bc2a7bfe1c
venktesh <venkysoma1998@gmail.com>
2020-06-18 13:22:29 +0530

    Added the accessor and scope functions

--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon; 
 
 class Post extends Model
 {
-    // Defining the Accesssor for image attribute over here that is called when model is called 
+    protected $dates = ['published_at'];
 
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    // Defining the Accesssor for image attribute over here that is called when model is called 
     public function getImageUrlAttribute($value)
     {
         $imageUrl = "";
@@ -18,9 +25,23 @@ class Post extends Model
             if(file_exists($imagePath)) 
             {
                 $imageUrl = asset("img/" . $this->image); # gets the relevant path of the image if the image exists.
-                
+
             }
         }
         return $imageUrl;
     }
+
+    public function getDateAttribute($value)
+    {
+        return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();     # will creats the date for human readble form.
+    }
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('published_at','desc');
+    }
+
+    // public function scopePublished($query)
+    // {
+    //     return $query->where("published_at","<=",Carbon::now());   # not working coz of php version might be
+    // } 
 }
