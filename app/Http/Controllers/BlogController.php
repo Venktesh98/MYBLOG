e1e734ad7c8037628e76bf369eaf7cc7e15e36a5
venktesh <venkysoma1998@gmail.com>
2020-06-20 17:04:17 +0530

    controller modified by adding the category

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Post;
+use App\Category;
 
 use Illuminate\Http\Request;
 
@@ -9,16 +10,15 @@ class BlogController extends Controller
 {
     
     public function index()
-    {
+    { 
+        $categories = Category::with('posts')->orderBy('title','asc')->get();
         // $post = Post::all();
         // \DB::enableQueryLog();    # for debugging the database query
 
         $post = Post::with('author')->latestFirst()->paginate(3);
-        // $post = Post::orderBy('created_at','asc')->paginate(5);
-        return view('blog.index')->with('posts_send',$post);   # remove return and add render () for DB query
+        return view('blog.index')->with('posts_send',$post)->with('categories',$categories);   # remove return and add render() at last in view statement for DB query
         // dd("blog msg");
         // dd(\DB::getQueryLog());
-
     }
 
     // public function show($id)
@@ -26,5 +26,11 @@ class BlogController extends Controller
     {
         // $post = Post::findOrFail($id);
         return view('blog.show')->with("posts",$postid);
+    }
+    public function category($id)
+    { 
+        $categories = Category::with('posts')->orderBy('title','asc')->get();
+        $post = Post::with('author')->latestFirst()->where('category_id',$id)->paginate(3);
+        return view('blog.index')->with('posts_send',$post)->with('categories',$categories);   # remove return and add render() at last in view statement for DB query
     }
 }
