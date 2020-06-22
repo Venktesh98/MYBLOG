a5d5a28f473f5856dfdef7a1b76ca8010a414ee5
venktesh <venkysoma1998@gmail.com>
2020-06-22 12:14:47 +0530

    Implemented the gravatar method in blade

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -20,7 +20,7 @@
                                 <ul class="post-meta-group">
                                     <li><i class="fa fa-user"></i><a href="{{ route('blog.author',$posts->author->slug) }}"> {{$posts->author->name}}</a></li>
                                     <li><i class="fa fa-clock-o"></i><time> {{$posts->date}}</time></li>
-                                    <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
+                                    <li><i class="fa fa-folder"></i><a href="{{ route('blog.category',$posts->category->slug) }}"> {{$posts->category->title }}</a></li>
                                     <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
                                 </ul>
                             </div>
@@ -32,21 +32,23 @@
 
                 <article class="post-author padding-10">
                     <div class="media">
+                        <?php $author = $posts->author; ?> <!-- defining a variable -->
                       <div class="media-left">
-                        <a href="#">
-                          <img alt="Author 1" src="/img/author.jpg" class="media-object">
+                        <a href="{{ route('blog.author',$author->slug) }}">
+                          <img alt="{{ $author->name }}" width="100" height= "100%" src="{{ $author->gravatar() }}" class="media-object">
                         </a>
                       </div>
                       <div class="media-body">
-                        <h4 class="media-heading"><a href="{{ route('blog.author',$posts->author->slug) }}">{{$posts->author->name}}</a></h4>
+                        <h4 class="media-heading"><a href="{{ route('blog.author',$author->slug) }}">{{$author->name}}</a></h4>
                         <div class="post-author-count">
-                          <a href="{{ route('blog.author',$posts->author->slug) }}">
+                          <a href="{{ route('blog.author',$author->slug) }}">
                               <i class="fa fa-clone"></i>
-                              <?php $postCount = $posts->author->posts->count() ?> <!-- defining a variable of postCount -->
+                              <?php $postCount = $author->posts->count() ?> <!-- defining a variable of postCount -->
                               {{ $postCount }} {{ str_plural('post',$postCount) }} <!-- helper function for plural -->
                           </a>
                         </div>
-                        {{ $posts->title }}
+                        
+                        {!! $author->bio_html !!}
                       </div>
                     </div>
                 </article>
