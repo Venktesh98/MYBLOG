5cca82624df2ec17e37c9428e77e6ac152335b92
venktesh <venkysoma1998@gmail.com>
2020-06-22 15:33:40 +0530

    added the faker object for bio field

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,25 +16,30 @@ class UsersTableSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
         DB::table('users')->truncate();
 
+        $faker = Factory::create(); # creating the faker object for the bio of user
+
         // generate 3 users/author
         DB::table('users')->insert([
             [
                 'name' => "venky",
                 'slug'=>"venky-soma",
                 'email' => "vgs@test.com",
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio'=>$faker->text(rand(250,300)),
             ],
             [
                 'name' => "omni",
                 'slug'=>"omni-metikel",
                 'email' => "omni@test.com",
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio'=>$faker->text(rand(250,300)),
             ],
             [
                 'name' => "Bharu",
                 'slug'=>"bharu-nakka",
                 'email' => "bharu@test.com",
-                'password' => bcrypt('secret')
+                'password' => bcrypt('secret'),
+                'bio'=>$faker->text(rand(250,300)),
             ],
         ]);
     }
