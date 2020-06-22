50fdb0773fdf334f5306d7ff571f5ec6aa8dd357
venktesh <venkysoma1998@gmail.com>
2020-06-22 12:14:07 +0530

    added the gravatar method for adding the default image of user

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -33,8 +34,22 @@ class User extends Authenticatable
         return $this->hasMany(Post::class , 'author_id');
     }
 
+    public function gravatar()   # it is used for inserting the image of user based on his email
+    {
+        $email = $this->email;
+        $default = "https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png";
+        $size = 20;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+    }
     public function getRouteKeyName()
     {
         return 'slug';
     }
+
+    public function getBioHtmlAttribute($value)
+    {
+        return $this->bio ? Markdown::convertToHtml(e($this->bio)) : NULL;  # e is laravel helper function that is used for security purpose.
+    }
+
 }
