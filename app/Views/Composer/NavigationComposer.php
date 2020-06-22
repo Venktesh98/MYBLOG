68f61e3bfb82db89b53aa4c5fb535d33ba78059b
venktesh <venkysoma1998@gmail.com>
2020-06-22 17:07:05 +0530

    code cleanup done by adding the view code in separte file

---
+++ app/Views/Composer/NavigationComposer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Views\Composer;
+
+use Illuminate\View\View;
+use App\Category;
+use App\Post;
+
+class NavigationComposer
+{
+    public function compose(View $view)
+    {
+        $this->composeCategories($view);
+
+        $this->composePopularposts($view);
+    }
+
+    private function composeCategories(View $view)
+    {
+        $categories = Category::with('posts')->orderBy('title','asc')->get();
+
+        $view->with('categories',$categories);
+    }
+
+    private function composePopularposts(View $view)
+    {
+        $popular_posts = Post::latestFirst()->popular()->take(3)->get();
+        $view->with('popularposts',$popular_posts);
+    }   
+}
