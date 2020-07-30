9553b19579b72efe719b7e83693179f5cd80ddad
venktesh <venkysoma1998@gmail.com>
2020-07-30 16:57:02 +0530

    Upload image done

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -11,11 +11,21 @@ use App\Http\Requests;
 class BlogController extends BackendController
 {
     protected $limit = 5;
+    protected $uploadPath;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        parent::__construct();   # for adding the middleware
+
+        $this->uploadPath =  public_path('img');
+    }
+
     public function index()
     {
         $posts = Post::with('author','category')->latest()->paginate($this->limit);
@@ -42,10 +52,48 @@ class BlogController extends BackendController
      */
     public function store(Requests\PostRequest $request)
     {
-        // here user() is to get the current user who makes the request.
-        $request->user()->posts()->create($request->all());   # can also be used $request->only('title') or any any attribute to get.
+        $data = $this->handleRequest($request);
 
+        // here user() is to get the current user who makes the request.
+        $request->user()->posts()->create($data);   # can also be used $request->only('title') or any any attribute to get.
+
+        // $post = new Post;
+        // $post->title = request('title');
+        // $post->slug = request('slug');
+        // $post->excerpt = request('excerpt');
+        // $post->body = request('body');
+        // $post->published_at = request('published_at');
+        // $post->category_id = request('category_id');
+        // $post->author_id = auth()->user()->id;
+
+        // $post->save();
         return redirect('/backend/blog')->with('success','Your Post was Created Successfully!');
+    }
+
+    // used for handling the request for image uploading
+    public function handleRequest($request)
+    {
+        $data = $request->all();
+
+        if($request->hasFile('image'))
+        {
+            // gets the image from html form
+            $image = $request->file('image');
+
+            // gets the image fileName
+            $fileName = $image->getClientOriginalName();
+
+            // destination of the image
+            $destination = $this->uploadPath;
+
+            // moves the image to the sepecified destination
+            $image->move($destination,$fileName);
+
+            // stores the image name in the database
+            $data['image'] = $fileName;
+        }
+
+        return $data;
     }
 
     /**
