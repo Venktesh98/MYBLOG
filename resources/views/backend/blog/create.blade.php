6ef9d902dd6022f72f5503238065b45c9e85d71e
venktesh <venkysoma1998@gmail.com>
2020-07-30 16:57:18 +0530

    Added the choose image property in form

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -30,7 +30,8 @@
               <div class="box-body">
                    {!! Form::model($post,[
                        'method'=>'POST',
-                       'route'=>'blog.store'
+                       'route'=>'blog.store',
+                       'files'=>TRUE              # This is for multipart for uploading the image
                    ]) !!}
 
                     <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}" >
@@ -89,6 +90,16 @@
                         @endif
 
                     </div>
+
+                    <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
+                      {!! Form::label('image','Feature Image') !!}
+                      {!! Form::file('image') !!}
+
+                      @if($errors->has('image'))
+                          <span class='help-block'>{{ $errors->first('image') }}</span>
+                      @endif
+
+                  </div>
 
                     {!! Form::submit('Create New Post',['class'=>'btn btn-primary'])!!}
 
