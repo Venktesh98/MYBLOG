5e087af6a1dd4fd3da2ccb1789910b4977e3cf61
venktesh <venkysoma1998@gmail.com>
2020-08-04 09:59:34 +0530

    done the edit and update conditions

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -138,7 +138,8 @@ class BlogController extends BackendController
      */
     public function edit($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        return view('backend.blog.edit',compact('post'));
     }
 
     /**
@@ -148,9 +149,13 @@ class BlogController extends BackendController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Requests\PostRequest $request, $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $data = $this->handleRequest($request);
+        $post->update($data);    # updates in the DB.
+
+        return redirect('/backend/blog')->with('success','Your Post was Updated Successfully!');
     }
 
     /**
