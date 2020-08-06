9f86c20534c0226a2dee2a9aaed2cf3682c30647
venktesh <venkysoma1998@gmail.com>
2020-08-06 11:42:40 +0530

    Added the category controller

---
+++ app/Http/Controllers/Backend/CategoriesController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Category;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class CategoriesController extends BackendController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $categories = Category::with('posts')->latest()->paginate($this->limit);
+       $categoriesCount = Category::count();
+
+       return view('backend.categories.index',compact('categories','categoriesCount'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Category $category)
+    {
+        return view('backend.categories.create',compact('category'));  
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Requests\CategoryStoreRequest $request)
+    {
+        # First Method
+        // $category = new Category;
+        // $category->title = $request->input('title');
+        // $category->slug = $request->input('slug');
+        // $category->save();
+
+        # Second one
+        Category::create($request->all());
+        return redirect('/backend/categories')->with('message','New Category Created Successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
