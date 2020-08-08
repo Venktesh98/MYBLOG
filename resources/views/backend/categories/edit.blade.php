7bf4190f83dc8829f0d5068e6b5b273a819892f5
venktesh <venkysoma1998@gmail.com>
2020-08-08 16:05:02 +0530

    Changes the all sepcs according to the Category

--- resources/views/backend/categories/edit.blade.php
+++ resources/views/backend/categories/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.backend.main')
 
-<title> MyBlog | Edit Post </title>
+<title> MyBlog | Edit category </title>
 @section('content')
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blogs
-        <small> <strong> Edit Post </strong> </small>
+        Categories
+        <small> <strong> Edit Category </strong> </small>
       </h1>
       <ol class="breadcrumb">
         <li>
@@ -15,30 +15,31 @@
         </li>
 
         <li>
-            <a href="{{route('blog.index')}}">Blog</a>
+            <a href="{{route('categories.index')}}">Category</a>
         </li>
-        <li class="active">Edit Post</li>
+        <li class="active">Edit Category</li>
       </ol>
     </section>
     
     <!-- Main content -->
     <section class="content">
         <div class="row">
-              {!! Form::model($post,[
+              {!! Form::model($category,[
                 'method'=>'PUT',
-                'route'=>['blog.update',$post->id],
-                'files'=>TRUE,              # This is for multipart for uploading the image
-                'id'=>'post-form'           # gets fired when javascript Event handler is invoked
+                'route'=>['categories.update',$category->id],
+                'files'=>TRUE,                          # This is for multipart for uploading the image
+                'id'=>'category-form'                   # gets fired when javascript Event handler is invoked
             ]) !!}
 
-          @include('backend.blog.form')       <!-- includes the Whole Form here -->
+          @include('backend.categories.form')       <!-- includes the Whole Form here -->
 
-          {!! Form::close() !!}               <!-- form close -->
+          {!! Form::close() !!}                     <!-- form close -->
         </div>
       <!-- ./row -->
     </section>
+</div>
     <!-- /.content -->  
 @endsection
 
-@include('backend.blog.script')         <!-- includes the all javascript code -->
+@include('backend.categories.script')         <!-- includes the all javascript code -->
 
