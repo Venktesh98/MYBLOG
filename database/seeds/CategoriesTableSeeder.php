5ebb8a2a62dcf5757d951b037f34666efb9e8b92
venktesh <venkysoma1998@gmail.com>
2020-08-08 16:03:37 +0530

    Adeed the uncategorised seed

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -34,6 +34,10 @@ class CategoriesTableSeeder extends Seeder
                 'title' => 'Photography',
                 'slug' => 'photography'
             ],
+            [
+                'title' => 'Uncategorized',
+                'slug' => 'uncategorized'
+            ],
         ]);
 
         // update the posts data
