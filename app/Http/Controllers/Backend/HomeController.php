20bee81e72d3cf941f9d64597522738b6cdc6494
venktesh <venkysoma1998@gmail.com>
2020-08-26 20:21:35 +0530

    Added the profilr section

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class HomeController extends BackendController
@@ -21,8 +22,24 @@ class HomeController extends BackendController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index() 
     {
-        return view('backend.home');
+        return view('backend.home.index');
+    }
+
+    public function edit(Request $request)
+    {
+        $user = $request->user();
+        return view('backend.home.edit',compact('user'));
+    }
+
+    public function update(Requests\UserProfileUpdateRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update($request->all());
+
+        return redirect()->back()->with('message','Profile updated successfully!');
     }
 }
+
