ab110d97465abf103ace3293db678e25b966538f
venktesh <venkysoma1998@gmail.com>
2020-08-27 14:20:39 +0530

    Added the alert file for the search process

---
+++ resources/views/blog/alert.blade.php
@@ -0,0 +1,17 @@
+@if($categoryName)     <!-- Category Name -->
+    <div class="alert alert-info">
+        <p> Category : <strong> {{ $categoryName }} </strong> </p>
+    </div>
+@endif
+
+@if($authorName)     <!-- Author Name -->
+    <div class="alert alert-info">
+    <p> Author : <strong> {{ $authorName }} </strong> </p>
+    </div>
+@endif
+
+@if($term = request('term'))     <!-- Search Name -->
+    <div class="alert alert-info">
+    <p> Search Results for : <strong> {{ $term }} </strong> </p>
+    </div>
+@endif
