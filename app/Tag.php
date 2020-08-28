4f1381f92eb9a3293048bce9d276f11d030f4a36
venktesh <venkysoma1998@gmail.com>
2020-08-29 00:48:47 +0530

    Added the getRouteKeyname insted of simple id passed slug

--- app/Tag.php
+++ app/Tag.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    public $table = 'tags';
 
     public function posts()
     {
         return $this->belongsToMany(Post::class);
+    }
+
+    // laravel by default finds by using id,so we are overriding id by slug here.
+    public function getRouteKeyName()  
+    {
+        return 'slug';
     }
 }
