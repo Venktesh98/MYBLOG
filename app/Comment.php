b04b3f01f5232a911535cc5a4cc57628e257a9fd
venktesh <venkysoma1998@gmail.com>
2020-09-02 15:48:31 +0530

    Added the accesssor for the date and body attributes in comments.blade.php

--- app/Comment.php
+++ app/Comment.php
@@ -4,11 +4,22 @@ namespace App;
 
 use App\Post;
 use Illuminate\Database\Eloquent\Model;
-
+use GrahamCampbell\Markdown\Facades\Markdown;
+ 
 class Comment extends Model
 {
     public function post()
     {
         return $this->belongsTo(Post::class);
+    }
+
+    public function getDateAttribute($value)
+    {
+        return is_null($this->created_at) ? '' : $this->created_at->diffForHumans();
+    }
+
+    public function getBodyHtmlAttribute($value)
+    {
+        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;  # e is laravel helper function that is used for security purpose.
     }
 }
